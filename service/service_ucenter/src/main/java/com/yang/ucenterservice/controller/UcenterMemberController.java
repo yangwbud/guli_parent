5045df2b2d206ba71307eee3c9b3e3a10ad75295
li4 <li4@125.com>
2020-03-29 22:03:05 +0800

    serivce_ucenter

---
+++ service/service_ucenter/src/main/java/com/yang/ucenterservice/controller/UcenterMemberController.java
@@ -0,0 +1,21 @@
+package com.yang.ucenterservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 会员表 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-03-29
+ */
+@RestController
+@RequestMapping("/ucenterservice/ucenter-member")
+public class UcenterMemberController {
+
+}
+
---
+++ service/service_ucenter/src/main/java/com/yang/ucenterservice/service/impl/UcenterMemberServiceImpl.java
@@ -0,0 +1,20 @@
+package com.yang.ucenterservice.service.impl;
+
+import com.yang.ucenterservice.entity.UcenterMember;
+import com.yang.ucenterservice.mapper.UcenterMemberMapper;
+import com.yang.ucenterservice.service.UcenterMemberService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 会员表 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-03-29
+ */
+@Service
+public class UcenterMemberServiceImpl extends ServiceImpl<UcenterMemberMapper, UcenterMember> implements UcenterMemberService {
+
+}
